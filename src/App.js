83ca8024964c7a1f64442959fe8f970dec840552
parimalnajan <63017672+parimalnajan@users.noreply.github.com>
2020-12-17 22:15:39 +0530

    fin.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  var [output,setOutput]=useState(' output here');
+  var [message,setMessage]=useState("Enter an Emoji or click on one to know its meaning");
+
+  var database={
+    
+    '☔':"rainy", 
+    "⚡":"thunderstorms",
+    "🌞":"sunny",
+    "🌀":"windy",
+    "⛄":"snowfall",
+    "⛅":"cloudy",
+   
+  
+  };
+
+  //let icons=["⛅","🌀","🌞","☔","⛄","⚡"];
+  let icons=Object.keys(database);
+  console.log(icons)
+
+  function emojiClickHandler(emoji){
+
+    console.log(emoji)
+    var meaning= database[emoji];
+    setOutput(meaning)
+
+     }
+
+
+  function inputHandler(e){   
+
+    var userInput=e.target.value;
+    var meaning= database[userInput];
+
+    
+    if(meaning===undefined){
+      meaning="we dont have in db";
+      setOutput(meaning);
+    }
+    else{
+      setOutput(meaning);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>Weather Icon Interpreter</h1>
+        <h5>{message}</h5>
+        <input placeholder="input here" onChange={inputHandler}></input>
+        
+        <p>{output}</p>
+
+        <h2>{icons.map((icon,index)=>
+        <span className="emoji-render" onClick={()=>emojiClickHandler(icon)}>{icon}</span>)}</h2>
+
+
       </header>
     </div>
   );
